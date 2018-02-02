3902403ee45eb35a5b67e9916b35b732631a0e48
Georges Younes <georges.r.younes@gmail.com>
2018-02-02 13:49:04 +0000

    git add test suites

---
+++ test/trcb_exp_SUITE.erl
@@ -0,0 +1,91 @@
+%% -------------------------------------------------------------------
+%%
+%% Copyright (c) 2018 Georges Younes.  All Rights Reserved.
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
+%% -------------------------------------------------------------------
+%%
+
+-module(trcb_exp_SUITE).
+-author("Georges Younes <georges.r.younes@gmail.com>").
+
+%% common_test callbacks
+-export([%% suite/0,
+         init_per_suite/1,
+         end_per_suite/1,
+         init_per_testcase/2,
+         end_per_testcase/2,
+         all/0]).
+
+%% tests
+-compile([nowarn_export_all, export_all]).
+
+-include("trcb_exp.hrl").
+
+-include_lib("common_test/include/ct.hrl").
+-include_lib("eunit/include/eunit.hrl").
+-include_lib("kernel/include/inet.hrl").
+
+-define(EVENT_NUMBER, 10).
+-define(EVENT_INTERVAL, 1000).
+
+%% ===================================================================
+%% common_test callbacks
+%% ===================================================================
+
+suite() ->
+    [{timetrap, {minutes, 2}}].
+
+init_per_suite(Config) ->
+    Config.
+
+end_per_suite(Config) ->
+    Config.
+
+init_per_testcase(Case, Config) ->
+    ct:pal("Beginning test case: ~p", [Case]),
+    Config.
+
+end_per_testcase(Case, Config) ->
+    ct:pal("Ending test case: ~p", [Case]),
+    Config.
+
+all() ->
+    [
+     test_trcb_base,
+     test_trcb_dots
+    ].
+
+%% ===================================================================
+%% tests
+%% ===================================================================
+
+test_trcb_base(_Config) ->
+    run_test(base, 7).
+
+test_trcb_dots(_Config) ->
+    run_test(dots, 7).
+
+%% @private
+run_test(Mode, NodeNumber) ->
+    Options = [{node_number, NodeNumber},
+               {trcb_exp_settings,
+                [{trcb_exp_node_number, NodeNumber},
+                 {trcb_exp_mode, Mode},
+                 {trcb_exp_default_event_interval, ?EVENT_INTERVAL},
+                 {trcb_exp_node_event_number, ?EVENT_NUMBER}]}],
+
+    trcb_exp_local_experiments_support:run_trcb(Options).
