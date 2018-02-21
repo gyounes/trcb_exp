1161d2a0b6b53d115289ae7e9b478c217578399e
Georges Younes <georges.r.younes@gmail.com>
2018-02-21 07:57:42 +0000

    update redis-sync scripts

---
+++ bin/emu_start-redis-sync.erl
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+POD_NAME=redis
+
+PORT=6379
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+METRICS_DIR=${DIR}/../priv/evaluation/metrics
+
+echo "    POD_NAME: ${POD_NAME}"
+echo "    PORT: ${PORT}"
+echo "    DIR: ${DIR}"
+echo "    METRICS_DIR: ${METRICS_DIR}"
+
+
+## TODO
+
+
+echo "[$(date +%T)] Port forwarding starting..."
+sleep 3
+
+cd "${DIR}"/..
+METRICS_DIR=${METRICS_DIR} "${DIR}"/redis-sync.erl
+
+echo "[$(date +%T)] All files downloaded!"
+
