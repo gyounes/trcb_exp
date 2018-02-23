a8046d67a48948724bb6ffcb8151f89c3adb7746
Georges Younes <georges.r.younes@gmail.com>
2018-02-23 07:33:31 +0000

    rebuild

--- bin/emu_exp-start.sh
+++ bin/emu_exp-start.sh
@@ -3,12 +3,7 @@
 ARG1=$1
 ARG2=$2
 
-if [ $2=true ]; then
-   ./rebar3 release -d
-else
-	cp -r ./_build ./_build$1
-fi
-
+./rebar3 release -d
 
 IP=$ARG1 \
 SYNCHRONIZER=$ARG2 \
@@ -19,4 +14,4 @@ DEFAULT_EVENT_INTERVAL=1000 \
 ORCHESTRATION=emulab \
 METRICS_STORE=redis \
 KEEP_ALIVE=false \
-./_build$1/default/rel/trcb_exp/bin/env
+./_build/default/rel/trcb_exp/bin/env
