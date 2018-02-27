1ad7354b3a28c6774292b8dc0445722f218df72e
Georges Younes <georges.r.younes@gmail.com>
2018-02-27 13:16:37 +0000

    update init emu script

--- emu_scripts/init.sh
+++ emu_scripts/init.sh
@@ -11,30 +11,43 @@ echo -e "Sync Name done ${GREEN}successfully${NC}" &&
 ReplicasNames=$(cat nodesNames | grep replica | awk '{print $4}' | tr '\n' ' ') &&
 echo -e "Replicas Names done ${GREEN}successfully${NC}" &&
 
-###
+#################################
 
-# ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" gyounes@"$StoreName".emulab.net 'cd trcb_exp; git pull; screen -S store -d -m redis-server' &&
-ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" gyounes@"$StoreName".emulab.net 'cd ~/trcb_exp; git pull;./rebar3 release -d; nohup redis-server > /dev/null 2>&1 &' &&
+# ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" gyounes@"$StoreName".emulab.net 'cd ~/trcb_exp; git pull;./rebar3 release -d; nohup redis-server > /dev/null 2>&1 &' &&
+# ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" gyounes@"$StoreName".emulab.net 'cd ~/trcb_exp ; git pull ; ./rebar3 release -d ; screen -S store -d -m redis-server' &&
+ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" gyounes@"$StoreName".emulab.net 'cd ~/trcb_exp ; git pull ; ./rebar3 release -d ; nohup redis-server > /dev/null 2>&1 &' &&
 echo -e "start store $StoreName done ${GREEN}successfully${NC}" &&
 
-###
+#################################
 
-# ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" gyounes@"$SyncName".emulab.net "cd trcb_exp; SyncIP=\$(ifconfig | grep addr:10.1.1. | awk '{print \$2}' | grep -Eo '[0-9\.]+'); echo \$SyncIP; screen -S sync -d -m ~/trcb_exp/bin/emu_exp-start.sh \$SyncIP true" &&
-ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" gyounes@"$SyncName".emulab.net "sudo mkdir /trcb; sudo chmod 777 /trcb; sudo cp -rf ~/trcb_exp/_build/; SyncIP=\$(ifconfig | grep addr:10.1.1. | awk '{print \$2}' | grep -Eo '[0-9\.]+'); echo \$SyncIP; ~/trcb_exp/bin/emu_exp-start.sh \$SyncIP true > /dev/null 2>&1 &" &&
+ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" gyounes@"$SyncName".emulab.net "sudo mkdir /trcb; sudo chmod 777 /trcb ; sudo cp -rf ~/trcb_exp/_build/ /trcb" &&
 echo -e "start sync $SyncName done ${GREEN}successfully${NC}" &&
-
-sleep 15
-
-###
 
 NodeNames=( $ReplicasNames )
 
 for Node in ${NodeNames[@]};
 do
-  # ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" gyounes@"$Node".emulab.net "cd trcb_exp; NodeIP=\$(ifconfig | grep addr:10.1.1. | awk '{print \$2}' | grep -Eo '[0-9\.]+'); echo \$NodeIP; screen -S \$NodeIP -d -m ~/trcb_exp/bin/emu_exp-start.sh \$NodeIP false" &&
-  ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" gyounes@"$Node".emulab.net "sudo mkdir /trcb; sudo chmod 777 /trcb; sudo cp -rf ~/trcb_exp/_build/; NodeIP=\$(ifconfig | grep addr:10.1.1. | awk '{print \$2}' | grep -Eo '[0-9\.]+'); echo \$NodeIP; ~/trcb_exp/bin/emu_exp-start.sh \$NodeIP false > /dev/null 2>&1 &" &&
+  ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" gyounes@"$Node".emulab.net "sudo mkdir /trcb ; sudo chmod 777 /trcb ; sudo cp -rf ~/trcb_exp/_build/ /trcb" &&
   echo -e "start replica $Node done ${GREEN}successfully${NC}"
-  sleep 15
+done
+
+sleep 7
+#################################
+
+# ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" gyounes@"$SyncName".emulab.net "sudo pkill -9 beam.smp; lsof -i:6866; SyncIP=\$(ifconfig | grep addr:10.1.1. | awk '{print \$2}' | grep -Eo '[0-9\.]+'); echo \$SyncIP; nohup ~/trcb_exp/bin/emu_exp-start.sh \$SyncIP true & > \$SyncIP" &&
+ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" gyounes@"$SyncName".emulab.net "sudo pkill -9 beam.smp; lsof -i:6866; sudo mkdir /trcb /priv; sudo chmod 777 /trcb /priv; sudo cp -rf ~/trcb_exp/_build/ /trcb ; SyncIP=\$(ifconfig | grep addr:10.1.1. | awk '{print \$2}' | grep -Eo '[0-9\.]+'); echo \$SyncIP; screen -S sync -d -m  ~/trcb_exp/bin/emu_exp-start.sh \$SyncIP true" &&
+echo -e "start sync $SyncName done ${GREEN}successfully${NC}" &&
+
+sleep 5
+
+for Node in ${NodeNames[@]};
+do
+  # ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" gyounes@"$Node".emulab.net "sudo pkill -9 beam.smp; lsof -i:6866; NodeIP=\$(ifconfig | grep addr:10.1.1. | awk '{print \$2}' | grep -Eo '[0-9\.]+'); echo \$NodeIP; nohup ~/trcb_exp/bin/emu_exp-start.sh \$NodeIP false & > \$NodeIP" &&
+  ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" gyounes@"$Node".emulab.net "sudo pkill -9 beam.smp; lsof -i:6866; sudo mkdir /trcb ; sudo chmod 777 /trcb ; sudo cp -rf ~/trcb_exp/_build/ /trcb ; NodeIP=\$(ifconfig | grep addr:10.1.1. | awk '{print \$2}' | grep -Eo '[0-9\.]+'); echo \$NodeIP; screen -S \$NodeIP -d -m  ~/trcb_exp/bin/emu_exp-start.sh \$NodeIP false" &&
+  echo -e "start replica $Node done ${GREEN}successfully${NC}"
+  sleep 7
 done
 
 echo -e "${GREEN}init.sh DONE${NC}"
+
+# tail -f ./*.log
