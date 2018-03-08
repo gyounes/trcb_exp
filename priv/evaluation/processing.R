f3b1fd152c5a59555dcd352d772e684c18406702
Georges Younes <georges.r.younes@gmail.com>
2018-03-08 23:29:58 +0000

    add R plot script for processing time

---
+++ priv/evaluation/processing.R
@@ -0,0 +1,36 @@
+# main function
+main <- function() {
+    source("cdfplot.R")
+  # source("linesplot.R")
+  # source("boxplot.R")
+  # draw!
+  metrics_dir <- "processed"
+  ylabel1 <- "Sending time (ms)"
+  ylabel2 <- "Delivering time (ms)"
+  xlabel1 <- "Number of Sent messages"
+  xlabel2 <- "Number of delivered messages"
+   logx <- TRUE
+    # logy <- FALSE
+
+  # list of simulations
+  simulations <- list.files(metrics_dir)
+  
+  for(i in 1:length(simulations)) {
+    simulation <- simulations[[i]]
+    dir <- paste(metrics_dir, simulation, sep="/")
+  
+    # latency local
+    key <- "latency_local"
+    output_file <- paste(simulation, "_", key, ".png", sep="")
+    splot(dir, simulation, key, output_file, ylabel1, logx)
+  	# splot(dir, simulation, key, output_file, ylabel1, xlabel1, logy)
+
+    # latency remote
+    key <- "latency_remote"
+    output_file <- paste(simulation, "_", key, ".png", sep="")
+    splot(dir, simulation, key, output_file, ylabel2, logx)
+  	# splot(dir, simulation, key, output_file, ylabel2, xlabel2, logy)
+  }
+}
+
+main()
